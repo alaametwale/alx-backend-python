9ac16ab2dbd100ad75d7eb80c200d3f8f1c4745e
alaametwale <alaameto86@gmail.com>
2025-11-17 00:55:40 +0200

    Feature: Defined Chat and Message models in chats app.

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.http import JsonResponse
+
+def index(request):
+    return JsonResponse({'message': 'Chats API working'})
