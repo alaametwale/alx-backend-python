c3c02e20d0df0e157c0f4a9952edc3c9376ffcd6
alaametwale <alaameto86@gmail.com>
2025-11-16 02:56:29 +0200

    Fix shebang line and position in test_utils

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,4 +1,11 @@
+
 #!/usr/bin/env python3
+#!/usr/bin/env python3
+
+import unittest
+from parameterized import parameterized
+from utils import access_nested_map
+
 import unittest
 from parameterized import parameterized
 from utils import access_nested_map
