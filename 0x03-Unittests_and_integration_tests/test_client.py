b09555a8997c2a5856dce71e0bcd728dcba9ab91
alaametwale <alaameto86@gmail.com>
2025-11-16 15:14:25 +0200

    Add parameterized test_has_license and integration tests for public_repos

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -2,6 +2,13 @@
 """
 Test for client.py
 """
+import sys
+import os
+
+# أضف مسار المشروع الرئيسي ليعرفه Python
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+ from utils import get_json, access_nested_map, memoize
+from client import GithubOrgClient
 
 import unittest
 from unittest.mock import patch, PropertyMock
