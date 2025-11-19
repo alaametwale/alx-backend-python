998a0beb5770d569a888eb3085092cb3af35da81
alaametwale <alaameto86@gmail.com>
2025-11-19 11:54:07 +0200

    Add ViewSets and API routes for chats app

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,16 +1,11 @@
-<<<<<<< HEAD
-from django.urls import path
-from . import views
+from django.urls import path, include
+from rest_framework import routers
+from .views import ConversationViewSet, MessageViewSet
+
+router = routers.DefaultRouter()
+router.register(r'conversations', ConversationViewSet, basename='conversations')
+router.register(r'messages', MessageViewSet, basename='messages')
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', include(router.urls)),
 ]
-=======
-# chats/urls.py
-from django.urls import path
-
-# لا يوجد مسارات API حتى الآن، لكن هذا الملف ضروري لتجنب خطأ الاستيراد.
-urlpatterns = [
-    # path('messages/', views.MessageListCreate.as_view(), name='message-list'),
-]
->>>>>>> 3a649449a61a17ea5d3214c20863e67fb301673f
