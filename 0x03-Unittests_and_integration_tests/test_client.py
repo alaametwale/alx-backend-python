532044559b95f49110f4d176edf282228cd20ded
alaametwale <132303984+alaametwale@users.noreply.github.com>
2026-01-06 00:22:44 +0200

    Update test_client.py

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,7 +1,8 @@
+#!/usr/bin/env python3
 import unittest
 from unittest.mock import patch
 from parameterized import parameterized
-from client import GithubOrgClient  # تأكد أن client.py موجود ويحتوي على GithubOrgClient
+from client import GithubOrgClient
 
 class TestGithubOrgClient(unittest.TestCase):
     """Test GithubOrgClient.org method"""
@@ -10,19 +11,14 @@ class TestGithubOrgClient(unittest.TestCase):
         ("google",),
         ("abc",)
     ])
-    @patch("client.get_json")  # نستخدم patch كديكور
+    @patch("client.get_json")
     def test_org(self, org_name, mock_get_json):
         """Test that GithubOrgClient.org returns the expected value"""
-
-        # إعداد القيمة المتوقعة ليُرجعها get_json
         expected = {"login": org_name}
         mock_get_json.return_value = expected
 
         client = GithubOrgClient(org_name)
         result = client.org()
 
-        # تحقق من أن الدالة أعادت القيمة المتوقعة
         self.assertEqual(result, expected)
-
-        # تحقق أن get_json استُدعيت مرة واحدة مع الرابط الصحيح
         mock_get_json.assert_called_once_with(f"https://api.github.com/orgs/{org_name}")
